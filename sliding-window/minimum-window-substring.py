3f63a56c337893771f442787ffbf7a4fffdc1b1d
Endriyas Zenagebriel <123267683+Endriyas21@users.noreply.github.com>
2024-09-21 23:29:39 +0300

    LC-minimum-window-substring [Runtime: N/A; Memory: N/A]

--- sliding-window/minimum-window-substring.py
+++ sliding-window/minimum-window-substring.py
@@ -1,22 +1,30 @@
 class Solution:
+    class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if t == "": return ""
+        if t == "":
+            return ""
+        
         need, have = len(t), 0
-        res, reslen = [-1,-1], float('inf')
+        res, reslen = [-1, -1], float('inf')
         window, count_t = Counter(), Counter(t)
         l = 0
+        
         for r in range(len(s)):
             char = s[r]
             window[char] = 1 + window.get(char, 0)
-            if char in count_t and count_t[char] == window[char]:
-                have+=1
-            while need == have:
-                if (r -l+1) < reslen:
-                    reslen = r-l+1
+            
+            if char in count_t and window[char] == count_t[char]:
+                have += 1
+            
+            while have == len(count_t):
+                if (r - l + 1) < reslen:
+                    reslen = r - l + 1
                     res = [l, r]
-                window[s[l]] -=1
-                if s[l] in count_t and count_t[s[l]] > window[s[l]]:
-                    have -=1
-                l+=1
+                
+                window[s[l]] -= 1
+                if s[l] in count_t and window[s[l]] < count_t[s[l]]:
+                    have -= 1
+                l += 1
+        
         l, r = res
         return "" if reslen == float('inf') else s[l:r+1]
