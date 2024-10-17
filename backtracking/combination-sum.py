04843842bee70d440f2e099a639476df64e0ce41
Endriyas Zenagebriel <123267683+Endriyas21@users.noreply.github.com>
2024-10-18 00:44:52 +0300

    LC-combination-sum [Runtime: 11 ms; Memory: 16.8 MB]

---
+++ backtracking/combination-sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        def dfs(i, temp, total):
+            if total == target:
+                res.append(temp.copy())
+                return
+            if i >= len(candidates) or total > target:
+                return
+            
+            temp.append(candidates[i])
+            dfs(i, temp, total +candidates[i])
+
+            temp.pop()
+            dfs(i+1, temp, total)
+
+        dfs(0, [], 0)
+        return res
