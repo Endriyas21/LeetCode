642ede46cc0d9d4f7d5d17096b2f628a92930d75
Endriyas Zenagebriel <123267683+Endriyas21@users.noreply.github.com>
2024-12-23 03:05:03 +0900

    LC-find-all-duplicates-in-an-array [Runtime: N/A; Memory: N/A]

---
+++ array/find-all-duplicates-in-an-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        res = []
+
+        for n in nums:
+            index = abs(n) -1
+
+            if nums[index] < 0:
+                res.append(abs(nums[index]))
+            else:
+                nums[index] = -nums[index]
+        
+        return res
