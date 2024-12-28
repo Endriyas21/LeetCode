b566fae41459c33f1fedf73a5de56cd47ce784a0
Endriyas Zenagebriel <123267683+Endriyas21@users.noreply.github.com>
2024-12-29 08:22:03 +0900

    LC-invert-binary-tree [Runtime: 0 ms; Memory: 17.5 MB]

--- breadth-first-search/invert-binary-tree.py
+++ breadth-first-search/invert-binary-tree.py
@@ -7,10 +7,10 @@
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
-            return None
+            return root
         temp = root.left
         root.left = root.right
         root.right = temp
         self.invertTree(root.left)
         self.invertTree(root.right)
-        return root 
+        return root
