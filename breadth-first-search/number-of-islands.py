bfb7bcebcfc2c775b3f9790ea66267a81a33ee25
Endriyas Zenagebriel <123267683+Endriyas21@users.noreply.github.com>
2025-01-02 10:22:39 +0900

    LC-number-of-islands [Runtime: 343 ms; Memory: 25.5 MB]

--- breadth-first-search/number-of-islands.py
+++ breadth-first-search/number-of-islands.py
@@ -3,23 +3,21 @@ class Solution:
         if not grid:
             return 0
         rows, cols = len(grid), len(grid[0])
+        island = 0
         visited = set()
-        island=0
-
         def bfs(r,c):
             q = deque()
             q.append((r,c))
             visited.add((r,c))
-
             while q:
-                row,col = q.popleft()
-                directions = [[1,0],[-1,0], [0,1],[0,-1]]
+                row, col = q.popleft()
+                directions = [[1,0],[-1,0],[0,1],[0,-1]]
                 for dr, dc in directions:
-                    r,c = row+dr, col+ dc
+                    r,c = row +dr, col +dc
+                    if (r in range(rows) and c in range(cols) and (r,c) not in visited and grid[r][c] == "1"):
 
-                    if (r in range(rows) and c in range(cols)) and grid[r][c] == "1" and (r,c) not in visited:
-                        q.append((r,c))
                         visited.add((r,c))
+                        q.append((r,c))
 
 
 
@@ -28,5 +26,8 @@ class Solution:
             for c in range(cols):
                 if grid[r][c] == "1" and (r,c) not in visited:
                     bfs(r,c)
-                    island+=1
-        return island 
+                    island +=1
+        return island
+
+
+
