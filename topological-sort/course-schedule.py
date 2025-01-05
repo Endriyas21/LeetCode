f046c951237c621af2be779f2b17ca2d80e94f1b
Endriyas Zenagebriel <123267683+Endriyas21@users.noreply.github.com>
2025-01-05 09:06:28 +0900

    LC-course-schedule [Runtime: 2 ms; Memory: 19.5 MB]

--- topological-sort/course-schedule.py
+++ topological-sort/course-schedule.py
@@ -3,19 +3,42 @@ class Solution:
         store = {i:[] for i in range(numCourses)}
         for crs, preq in prerequisites:
             store[crs].append(preq)
-
+        
         visited = set()
         def dfs(crs):
+
             if crs in visited:
                 return False
             if store[crs] == []:
                 return True
             visited.add(crs)
-            for preq in store[crs]:
-                if not dfs(preq): return False
+            for nei in store[crs]:
+                if not dfs(nei): return False
             store[crs] = []
             visited.remove(crs)
             return True
+
         for crs in range(numCourses):
             if not dfs(crs): return False
-        return True
+        return True 
+        # visited = set()
+        # completed = set()
+        
+        # def dfs(crs):
+        #     if crs in visited:
+        #         return False
+        #     if crs in completed:
+        #         return True
+            
+        #     visited.add(crs)
+        #     for nei in store[crs]:
+        #         if not dfs(nei):
+        #             return False
+        #     visited.remove(crs)
+        #     completed.add(crs)
+        #     return True
+
+        # for crs in range(numCourses):
+        #     if not dfs(crs):
+        #         return False
+        # return True
