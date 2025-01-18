c2f4ab84e4978c9da641f8f970468a044cdcff1c
Endriyas Zenagebriel <123267683+Endriyas21@users.noreply.github.com>
2025-01-18 13:50:35 +0900

    LC-jump-game [Runtime: N/A; Memory: N/A]

--- greedy/jump-game.py
+++ greedy/jump-game.py
@@ -1,7 +1,9 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        goal = len(nums)-1
-        for i in range(len(nums)-2, -1,-1):
-            if i + nums[i] >= goal:
-                goal = i
-        return goal == 0
+        curr = 0
+        for i in range(len(nums)):
+            curr = i + nums[i]
+            if curr == len(nums)-1:
+                return True
+        #print(curr)
+        return curr == len(nums)-1
