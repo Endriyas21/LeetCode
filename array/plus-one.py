69dd41343f5c48d9e32db5d781a1c134c59552ec
Endriyas Zenagebriel <123267683+Endriyas21@users.noreply.github.com>
2025-01-19 16:27:37 +0900

    LC-plus-one [Runtime: 0 ms; Memory: 17.6 MB]

--- array/plus-one.py
+++ array/plus-one.py
@@ -1,9 +1,8 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        for i in range(len(digits) - 1, -1, -1):
+        for i in range(len(digits)-1, -1, -1):
             if digits[i] < 9:
-                digits[i] += 1
+                digits[i] +=1
                 return digits
-            digits[i] = 0  # Set the current digit to 0 and continue
-        # If the loop completes, all digits were 9, so we add a leading 1
+            digits[i] = 0
         return [1] + digits
