53654b9ca73a9274d44edf5c64ba08150a2db41d
Endriyas Zenagebriel <123267683+Endriyas21@users.noreply.github.com>
2025-02-15 08:51:01 +0900

    LC-longest-consecutive-sequence [Runtime: N/A; Memory: N/A]

--- array/longest-consecutive-sequence.java
+++ array/longest-consecutive-sequence.java
@@ -9,10 +9,12 @@ class Solution {
 
         for (int n: nums){
             if(!store.contains(n-1)){
+                int currentNum = n;
                 int currLen = 1;
 
-                while (store.contains(n+ currLen)){
+                while (store.contains(n+ 1)){
                     currLen++;
+                    currentNum++;
                 }
                 res = Math.max(currLen, res); 
 
