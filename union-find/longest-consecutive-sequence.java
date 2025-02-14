8032b6169896252f959c30640f1f3d059fc298cc
Endriyas Zenagebriel <123267683+Endriyas21@users.noreply.github.com>
2025-02-15 08:47:00 +0900

    LC-longest-consecutive-sequence [Runtime: N/A; Memory: N/A]

---
+++ union-find/longest-consecutive-sequence.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if (nums.length == 0){
+            return 0;
+        }
+        Set<Integer> store = new HashSet<>();
+        for (int num: nums){
+            store.add(num);
+        }
+
+        int res = 0;
+
+        for (int n: nums){
+            if(!store.contains(n-1)){
+                int currLen = 1;
+
+                while (store.contains(n+ currLen)){
+                    currLen++;
+                }
+                res = Math.max(currLen, res); 
+
+            }
+        }
+        return res;
+        
+    }
+}
