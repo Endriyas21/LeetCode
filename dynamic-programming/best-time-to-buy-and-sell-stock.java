6f5a1ce58ace4672e57abcde524d23418e29c824
Endriyas Zenagebriel <123267683+Endriyas21@users.noreply.github.com>
2025-02-15 14:59:18 +0900

    LC-best-time-to-buy-and-sell-stock [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int l = 0;
+        int maxProfit = 0;
+        for (int r=0; r < prices.length-1; r++){
+            if (prices[l] > prices[r]){
+                l = r;
+            }
+            maxProfit = Math.max(maxProfit, prices[r] - prices[l]);
+
+        }
+        return maxProfit;
+        
+    }
+}
