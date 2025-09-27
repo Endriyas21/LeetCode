a774a838e37a89f4fcb1dbc6223b6e8108f5663d
Endriyas Zenagebriel <123267683+Endriyas21@users.noreply.github.com>
2025-09-26 21:48:40 -0700

    LC-maximum-number-of-fish-in-a-grid [Runtime: 83 ms; Memory: 18.1 MB]

--- array/maximum-number-of-fish-in-a-grid.py
+++ array/maximum-number-of-fish-in-a-grid.py
@@ -13,11 +13,11 @@ class Solution:
                 directions = [[1,0],[-1,0],[0,1],[0,-1]]
                 for dr, dc in directions:
                     newRow, newCol = r+dr, c+dc
-                    if (newRow in range(rows) and newCol in range(cols) and grid[newRow][newCol] != 0):
+                    if (newRow in range(rows) and newCol in range(cols) and (newRow, newCol) not in seen and grid[newRow][newCol] != 0):
                         temp_fish+=grid[newRow][newCol]
                         seen.add((newRow,newCol))
                         q.append((newRow, newCol))
-                return temp_fish
+            return temp_fish
 
         for r in range(rows):
             for c in range(cols):
