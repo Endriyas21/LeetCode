3dc922e915d81425e4218dd1eb729ac027311714
Endriyas Zenagebriel <123267683+Endriyas21@users.noreply.github.com>
2025-09-27 21:56:19 -0700

    LC-rotting-oranges [Runtime: 8 ms; Memory: 17.7 MB]

--- array/rotting-oranges.py
+++ array/rotting-oranges.py
@@ -1,27 +1,32 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        q = deque()
-        fresh, time = 0, 0
         rows, cols = len(grid), len(grid[0])
+        seen = set()
+        q = deque()
+        fresh = 0
+        def addRows(r, c):
+            nonlocal fresh
+            if (r in range(rows) and c in range(cols) and (r,c) not in seen and grid[r][c] == 1):
+                seen.add((r,c))
+                q.append((r,c))
+                fresh-=1
+                grid[r][c] = 2
+        
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == 1:
+                if (grid[r][c] == 2):
+                    q.append((r,c))
+                    seen.add((r,c))
+                elif (grid[r][c] == 1):
                     fresh+=1
-                if grid[r][c] == 2:
-                    q.append([r,c])
-        directions = [[1,0],[-1,0],[0,1],[0,-1]]
-        while q and fresh > 0:
+        time = 0
+        while q and fresh>0:
             for i in range(len(q)):
-                row, col = q.popleft()
-                for dr, dc in directions:
-                    r,c = row+dr, col+dc
-                    if (r not in range(rows) or c not in range(cols) or grid[r][c] != 1):
-                        continue
- 
-                    grid[r][c] = 2
-                    q.append([r,c])
-                    fresh-=1
+                r, c = q.popleft()
+                addRows(r+1,c)
+                addRows(r-1,c)
+                addRows(r,c+1)
+                addRows(r,c-1)
             time+=1
-        return time if fresh == 0 else -1
-                    
+        return time if not fresh else -1
                 
