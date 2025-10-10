57e47a032769e2508285bed00e04598e1660a0df
Endriyas Zenagebriel <123267683+Endriyas21@users.noreply.github.com>
2025-10-09 23:31:24 -0700

    LC-spiral-matrix [Runtime: 0 ms; Memory: 17.7 MB]

--- simulation/spiral-matrix.py
+++ simulation/spiral-matrix.py
@@ -3,8 +3,8 @@ class Solution:
         top, bottom = 0, len(matrix)
         left, right = 0, len(matrix[0])
         res = []
-
         while left < right and top < bottom:
+
             for i in range(left, right):
                 res.append(matrix[top][i])
             top+=1
@@ -12,18 +12,13 @@ class Solution:
             for i in range(top, bottom):
                 res.append(matrix[i][right-1])
             right -=1
-
-            if not (left < right and top < bottom):
+            if not(left < right and top < bottom):
                 break
-
-            for i in range(right-1, left-1, -1):
-                res.append(matrix[bottom -1][i])
+            for i in range(right-1, left -1, -1):
+                res.append(matrix[bottom-1][i])
             bottom -=1
 
-            for i in range(bottom -1, top-1, -1):
+            for i in range(bottom-1, top-1, -1):
                 res.append(matrix[i][left])
-            left +=1
+            left+=1
         return res
-            
-
-    
